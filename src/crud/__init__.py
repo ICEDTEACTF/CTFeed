68502d2a2155480cbce6c013234c3cde99961e23
pictures2333 <m0987529191@gmail.com>
2026-02-19 22:51:50 +0800

    refactor(event): split read_event into read_event_one and read_event_many

--- src/crud/__init__.py
+++ src/crud/__init__.py
@@ -1,8 +1,8 @@
 from .config import create_or_update_config, read_config
 from .user import create_user, read_user, update_user
 from .event import (
-    try_lock_event, unlock_event,
+    unlock_event,
     NotFoundError, LockedError,
     join_event, delete_user_in_event,
-    create_event, read_event, read_ctfime_events_need_archive, update_event,
+    create_event, read_event_one, read_event_many, read_ctfime_events_need_archive, update_event,
 )
